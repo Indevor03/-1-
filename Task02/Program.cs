25550ba5e93f92818cb27bbab3b5ca02860d1f32
Indevor03 <124826427+Indevor03@users.noreply.github.com>
2023-03-09 00:06:38 +0300

    Папки домашнее задание

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// 1. На вход принимает два числа
+// 2. Определяет наибольшее из них
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Ввод a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввод b");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if (numbera < numberb)
+{
+    Console.WriteLine("Наибольшее число b, наименьшее число a");
+}
+else
+{
+    Console.WriteLine("Наибольшее число а, нименьшее число b");
+}
+
---
+++ Task04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая
+// 1. На вход принимает три числа
+// 2. Определяет наибольшее из них
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Ввод a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввод b");
+int numberb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввод c");
+int numberc = Convert.ToInt32(Console.ReadLine());
+if (numbera > numberb && numbera > numberc)
+{
+    Console.WriteLine("Наибольшее число a");
+}
+else
+if (numberb > numbera && numberb > numberc)
+{
+    Console.WriteLine("Наибольшее число b");
+}
+else
+if (numberc > numbera && numberc > numberb)
+{
+    Console.WriteLine("Наибольшее число c");
+}
+
