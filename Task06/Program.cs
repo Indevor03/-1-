26d1d2349a223e6bf5bf06403b51b0c65e01499e
Indevor03 <124826427+Indevor03@users.noreply.github.com>
2023-03-09 00:07:48 +0300

    Добавление папок

---
+++ Task06/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая
+// 1. Принимает на вход число
+// 2. Выводит, является ли число чётным (делится ли оно на два без остатка)
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Ввести число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1 || number <= 1)
+{
+    Console.WriteLine("Нечет");
+}
+else
+{
+    Console.WriteLine("Чёт");
+}
